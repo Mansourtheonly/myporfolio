3d709e2613a12c00461f51a2c62aed5af844e3ce
Mehdi Mansour <73033099+Mansourtheonly@users.noreply.github.com>
2025-04-14 19:44:43 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="text-center my-2 text-zinc-700 dark:text-zinc-400  slide-top-2750">
       Developed By{" "}
       <span className="capitalize font-bold text-zinc-800 dark:text-zinc-500">
-        Touaria Sami
+        Mehdi Mansour
       </span>
     </footer>
   );
